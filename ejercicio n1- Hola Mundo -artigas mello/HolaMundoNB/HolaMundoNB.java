5a6e7e58b44499b90a01db598163f5960b1e2860
Carlos Melllo <aurinegro1970@gmail.com>
2023-04-07 19:43:33 -0300

    ejercicio n1

---
+++ ejercicio n1- Hola Mundo -artigas mello/HolaMundoNB/HolaMundoNB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundonb;
+
+/**
+ *
+ * @author aurin
+ */
+public class HolaMundoNB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
