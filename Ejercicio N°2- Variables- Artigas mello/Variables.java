bc426712b805509c81f68a3a6b00ea3184498638
Carlos Melllo <aurinegro1970@gmail.com>
2023-04-08 19:42:58 -0300

    ejercicios nro2

---
+++ Ejercicio N°2- Variables- Artigas mello/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+  public static void main (String[] args) {
+   int Var1 = 25;
+   int Var2 = 35;
+   int Var3 = Var1 + Var2;
+  System.out.println("El resultado es "
++ Var3);
+}
+}
